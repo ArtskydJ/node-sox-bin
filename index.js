d40ea4268a09365b52b71113984d2adf47b21e1e
Adam Jones <domdomegg+git@gmail.com>
2024-12-25 05:20:28 +0000

    Add Apple Silicon (darwin_arm64) support

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
 	win32_x64: __dirname + '\\vendor\\windows\\sox.exe',
 	darwin_ia32: __dirname + '/vendor/osx/sox',
 	darwin_x64: __dirname + '/vendor/osx/sox',
+	darwin_arm64: __dirname + '/vendor/osx/sox',
 	freebsd_ia32: __dirname + '/vendor/freebsd/i386/sox',
 	freebsd_x64: __dirname + '/vendor/freebsd/amd64/sox',
 	linux_ia32: __dirname + '/vendor/fedora/i686/sox',
