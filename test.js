5f593870b7dc03fc7324debfd44f99869ec117d8
ArtskydJ <1833684+ArtskydJ@users.noreply.github.com>
2024-12-26 11:36:36 -0600

    Stop using tape in tests
    I hadn't defined it as a dev dep

--- test.js
+++ test.js
@@ -1,21 +1,23 @@
-var test = require('tape')
+var t = require('node:assert')
 var soxPath = require('./index.js')
 var cp = require('child_process')
 
-test('returned file path', function (t) {
-	t.equal(typeof soxPath, 'string')
-	t.ok(/vendor.+sox/.test(soxPath), 'found "vendor" and "sox" in the path')
-	t.notEqual(soxPath[0], '.', 'path is not relative')
-	t.end()
+let done = false
+
+t.equal(typeof soxPath, 'string')
+t.ok(/vendor.+sox/.test(soxPath), 'found "vendor" and "sox" in the path')
+t.notEqual(soxPath[0], '.', 'path is not relative')
+
+cp.exec(soxPath + ' --version', function (err, stdout, stderr) {
+	t.equal(err, null, err ? err.message : 'no error')
+
+	t.notEqual(stdout.length, 0, 'something returned on stdout')
+	t.equal(stderr.length, 0, 'nothing returned on stderr')
+	t.ok(/sox/i.test(stdout.toString()), 'the string "sox" is found in stdout')
+
+	done = true
 })
 
-test('sox executable works', function (t) {
-	cp.exec(soxPath + ' --version', function (err, stdout, stderr) {
-		t.notOk(err, err ? err.message : 'no error')
-
-		t.ok(stdout.length, 'something returned on stdout')
-		t.notOk(stderr.length, 'nothing returned on stderr')
-		t.ok(/sox/i.test(stdout.toString()), 'the string "sox" is found in stdout')
-		t.end()
-	})
+process.on('exit', function () {
+	t.ok(done, 'test completed')
 })
